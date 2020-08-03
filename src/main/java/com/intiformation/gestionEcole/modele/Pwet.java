7cf5f56708cd56a9c22402064041073083658f31
pif leprovost <pf.leprovost21310@orange.fr>
2020-08-03 10:34:42 +0200

    ajout classe pwet

---
+++ src/main/java/com/intiformation/gestionEcole/modele/Pwet.java
@@ -0,0 +1,5 @@
+package com.intiformation.gestionEcole.modele;
+
+public class Pwet {
+
+}
--- src/main/java/com/intiformation/gestionEcole/modele/Test.java
+++ src/main/java/com/intiformation/gestionEcole/modele/Test.java
@@ -1,5 +1,8 @@
 package com.intiformation.gestionEcole.modele;
 
 public class Test {
-
+	
+	/*__________________________ props __________________________*/
+	public String cookie;
+	
 }
